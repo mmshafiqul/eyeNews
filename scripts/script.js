7bf37be528fb2f104436be39033ce5a8b52a73c6
mmshafiqul <mmshafiqul6@gmail.com>
2024-05-12 21:36:30 +0600

    owl-slider added and responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,28 @@
 const d = new Date();
 const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-
 const formattedDate = d.toLocaleDateString(undefined, options);
 document.getElementById("day").innerHTML = formattedDate;
+
+// owl-careousel
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center: true,
+        nav:true,
+        autoplay:true, 
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1020:{
+                items:4,
+            }
+        }
+    })
+})
